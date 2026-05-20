96bef1b4c5c76d4b27fd1b8cf56fe593f472b985
Beaskyy <afeez.akintobi@yahoo.com>
2026-05-20 17:42:37 +0100

    routes

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -13,9 +14,9 @@ const __dirname = dirname(__filename);
 
 // Middleware
 // This line allows express to parse JSON data from incoming request bodies
-app.use(express.json())
+app.use(express.json());
 // This line allows express to parse URL-encoded data from incoming request bodies
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 // Serving up the html file from the /public directory
 // Tell express to serve all files from the public folder as static assets / files. Any requests for the css files will be resolved to the public directory
 app.use(express.static(path.join(__dirname, "../public")));
@@ -23,6 +24,9 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// Routes
+app.use("/auth", authRoutes);
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
