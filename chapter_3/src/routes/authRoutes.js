6d3c5a9d7b7c8ad69eebefb0ef0365cc1f02d812
Beaskyy <afeez.akintobi@yahoo.com>
2026-05-22 22:28:21 +0100

    authentication

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -6,8 +6,54 @@ import db from "../db.js";
 const router = express.Router();
 
 router.post("/register", (req, res) => {
+    const { username, password } = req.body
+    const hashedPassword = bcrypt.hashSync(password, 8)
+
+    // save the new user and hashed password to the db
+    
+    try {
+        const insertUser = db.prepare(`INSERT into users (username, password) 
+        VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        // Now we have a user, I want to add their first todo for them
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) 
+        VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid}, process.env.JWT_SECRET, { expiresIn: "24h"})
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.status(503)
+    }
 })
 
-router.post("/login", (req, res) => {})
+router.post("/login", (req, res) => {
+    const {username, password} = req.body
+
+    try {
+        const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`)
+        const user = getUser.get(username)
+
+        if(!user) {
+            return res.status(404).send({ message: "User not found"})
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        if(!passwordIsValid) {
+            return res.status(401).send({ message: "Invalid password"})
+        }
+
+        // If everything goes well
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "24h"})
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.status(503)
+    }
+})
 
 export default router;
