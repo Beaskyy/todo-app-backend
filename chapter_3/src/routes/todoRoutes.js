afd647e648b4141d419934abd2a70cc977ee1bc0
Beaskyy <afeez.akintobi@yahoo.com>
2026-05-25 21:03:29 +0100

    update

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -8,6 +8,7 @@ router.get("/", (req, res) => {
     const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
     const todos = getTodos.all(req.userId)
     res.json({ todos })
+    
 })
 
 // Create a new todo
