37fb0878a7dc98e48641a0ca5f9314bd39dd76ae
Beaskyy <afeez.akintobi@yahoo.com>
2026-05-26 12:09:26 +0100

    chore: remove unused label-related query logic from todo routes

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -8,24 +8,6 @@ router.get("/", (req, res) => {
     const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
     const todos = getTodos.all(req.userId)
     res.json({ todos })
-
-    // new stuff
-    try {
-        const query = `
-            SELECT 
-                t.*,
-                l.id AS label_id,
-                l.name AS label_name,
-                l.color_hex AS label_color
-            FROM todos t
-            LEFT JOIN todo_labels tl ON t.id = tl.todo_id
-            LEFT JOIN labels l ON tl.label_id = l.id
-            WHERE t.user_id = ?
-            ORDER BY t.created_at DESC
-        `;
-        const stmt = db.prepare(query);
-        const rows = stmt.all(req.userId);
-        // ...rest is the same
 })
 
 // Create a new todo
