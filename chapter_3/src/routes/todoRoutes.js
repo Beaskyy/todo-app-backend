a5374d1f0652451604e597be537dadbc4e9384ff
Beaskyy <afeez.akintobi@yahoo.com>
2026-06-15 13:45:02 +0100

    feat: implement full CRUD functionality for todo routes and update REST client tests

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -1,22 +1,45 @@
-import express from "express"
-import db from "../db.js"
+import express from "express";
+import db from "../db.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // Get all todos for logged-in user
 router.get("/", (req, res) => {
-    const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
-    const todos = getTodos.all(req.userId)
-    res.json({ todos })
-})
+  const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`);
+  const todos = getTodos.all(req.userId);
+  res.json({ todos });
+});
 
 // Create a new todo
-router.post("/", (req, res) => {})
+router.post("/", (req, res) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(
+    `INSERT INTO todos(user_id, task) VALUES (?, ?)`,
+  );
+  const result = insertTodo.run(req.userId, task);
+
+  res.json({ id: result.lastInsertRowid, task, completed: 0 });
+});
 
 // Update a todo
-router.put("/:id", (req, res) => {})
+router.put("/:id", (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
+
+    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    updatedTodo.run(completed, id)
+
+    res.json({ message: "Todo updated successfully" })
+});
 
 // Delete a todo
-// router.delete("/:id", (req, res) => {})
+router.delete("/:id", (req, res) => {
+    const { id } = req.params
 
-export default router
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? and user_id = ?`)
+    deleteTodo.run(id, req.userId)
+
+    res.json({ message: "Todo deleted" })
+})
+
+export default router;
